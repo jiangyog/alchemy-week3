8ee652526872512441dc1ca3961ead6ebadf46d1
yog2012 <yogcoming@126.com>
2022-11-06 22:09:47 +0800

    remove the key

--- ChainBattled/hardhat.config.js
+++ ChainBattled/hardhat.config.js
@@ -6,8 +6,8 @@ module.exports = {
   solidity: "0.8.10",
   networks: {
     mumbai: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/yZ3kArQKHA__TNss-iVluK-jxCFkHO9u",// process.env.TESTNET_RPC
-      accounts: ["065ebcdd980df35ef8ec1b1753ad8be51c63042f8c15dcd305d682c7502dfeba"]//[process.env.PRIVATE_KEY]
+      url: process.env.TESTNET_RPC,// 
+      accounts: [process.env.PRIVATE_KEY]
     },
   },
   etherscan: {
